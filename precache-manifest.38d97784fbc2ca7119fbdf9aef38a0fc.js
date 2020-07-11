66954e01a3d08bf74b8ce19cc9411e3f99921df3
Robert Rapacz <bercik468@gmail.com>
2020-07-11 16:40:40 +0200

    Updates

---
+++ precache-manifest.38d97784fbc2ca7119fbdf9aef38a0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "703fedb8fdf079db342164fff2fae689",
+    "url": "/Bookstore/index.html"
+  },
+  {
+    "revision": "2c742297587715ddd76d",
+    "url": "/Bookstore/static/css/main.27eda9a4.chunk.css"
+  },
+  {
+    "revision": "6045115d2787a440d190",
+    "url": "/Bookstore/static/js/2.4683697e.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/Bookstore/static/js/2.4683697e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c742297587715ddd76d",
+    "url": "/Bookstore/static/js/main.fa75997a.chunk.js"
+  },
+  {
+    "revision": "67d589cb55447e1f1f35",
+    "url": "/Bookstore/static/js/runtime-main.d341e1d1.js"
+  }
+]);
